ad3078f0f33c9aaf590b3d6cff0487233bc4b913
Oleksiy Litvinov <leetwinoff92@gmail.com>
2023-09-13 11:55:20 -0500

    Added router for TelegramUserViewSet

---
+++ stylist_bot/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from stylist_bot.views import TelegramUserViewSet
+
+router = DefaultRouter()
+router.register(r'telegram-users', TelegramUserViewSet)
+
+urlpatterns = [
+    path("/api/", include(router.urls))
+]
