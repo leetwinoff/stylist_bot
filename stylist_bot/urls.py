e43944cb6a3c029752dad5bd12a3cf3a5384a694
Oleksiy Litvinov <leetwinoff92@gmail.com>
2023-09-13 18:10:05 -0500

    Updated typing error

--- stylist_bot/urls.py
+++ stylist_bot/urls.py
@@ -6,5 +6,5 @@ router = DefaultRouter()
 router.register(r'telegram-users', TelegramUserViewSet)
 
 urlpatterns = [
-    path("/api/", include(router.urls))
+    path("api/", include(router.urls))
 ]
